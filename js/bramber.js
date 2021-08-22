06045f992cac77ce49e88005e39b5a6c0d2ac53a
Bramberrr <Bramberrr@example.com>
2021-08-22 23:53:26 +0800

    Site updated: 2021-08-22 23:53:25

--- js/bramber.js
+++ js/bramber.js
@@ -1,1 +1,29 @@
-"use strict";for(var mobile_sidebar_menus=document.getElementById("mobile-sidebar-menus"),menus_item_child=mobile_sidebar_menus.getElementsByClassName("menus_item_child"),menus_expand=mobile_sidebar_menus.getElementsByClassName("expand"),i=0;i<menus_item_child.length;i++)menus_item_child[i].style.display="none",menus_expand[i].className+=" closed";var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="Hey!Look at me!",clearTimeout(titleTime)):(document.title="Welcome to my page"+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
+
+
+/* 手机客户端导航栏默认隐藏 */
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " closed";
+}
+
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = 'Hey!Look at me!';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = 'Welcome to my page' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
